9691cc7bdd400d5b428fcb8dc0ea744c17dc36e6
jyoti2424 <jyotisahib2013@gmail.com>
2025-01-14 19:22:57 +0530

    Main file updated with List collection

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
-class Student {
+class Student implements Comparable<Student>{
     private String name;
     private double gpa;
 
@@ -21,7 +18,31 @@ class Student {
         return gpa;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Double.compare(gpa, student.gpa) == 0 && Objects.equals(name, student.name);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, gpa);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", gpa=" + gpa +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return Double.compare(o.getGpa(),this.getGpa());
+    }
 }
 
 public class Main {
@@ -121,7 +142,7 @@ public class Main {
        list.add(3);
 
        list.sort((a, b) -> a - b);
-       System.out.println(list);*/
+       System.out.println(list);
 
        List<Student> students = new ArrayList<>();
        students.add(new Student("Charlie", 3.5));
@@ -134,10 +155,15 @@ public class Main {
        students.sort(comparator);
        for (Student s : students) {
            System.out.println(s.getName() + ": " + s.getGpa());
-       }
+       }*/
 
+       List<Student> students = new ArrayList<>();
+       students.add(new Student("Charlie", 3.5));
+       students.add(new Student("Bob", 3.7));
+       students.add(new Student("Alice", 3.5));
+       students.add(new Student("Akshit", 3.9));
 
-
-
+        Collections.sort(students);
+        System.out.println(students);
    }
 }
